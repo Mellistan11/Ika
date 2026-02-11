0ce6f6ec5162b98dfc6df4612fbbed24a05b3269
Mellistan11 <melvinsjoholm0@gmail.com>
2026-02-11 12:39:26 +0100

    0.1.51

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -10,20 +10,25 @@
 
   const menuButton = document.getElementById("menu-button");
   const dropdownMenu = document.getElementById("dropdown-menu");
+  const dropdownShadow = document.getElementById("dropdown-shadow")
   let menuOpen = false;
 
   function openMenu() {
     menuButton.classList.add("menu-button-open");
-    dropdownMenu.classList.remove("dropdown-menu-hidden");
-    dropdownMenu.classList.add("dropdown-menu-shown");
+    dropdownMenu.classList.toggle("dropdown-menu-hidden");
+    dropdownMenu.classList.toggle("dropdown-menu-shown");
+    dropdownShadow.classList.toggle("dropdown-menu-hidden");
+    dropdownShadow.classList.toggle("dropdown-menu-shown");
 
     menuOpen = true;
   }
 
   function closeMenu() {
     menuButton.classList.remove("menu-button-open");
-    dropdownMenu.classList.remove("dropdown-menu-shown");
-    dropdownMenu.classList.add("dropdown-menu-hidden");
+    dropdownMenu.classList.toggle("dropdown-menu-shown");
+    dropdownMenu.classList.toggle("dropdown-menu-hidden");
+    dropdownShadow.classList.toggle("dropdown-menu-shown");
+    dropdownShadow.classList.toggle("dropdown-menu-hidden");
 
     menuOpen = false;
   }
@@ -39,28 +44,7 @@
 
 
 
-// Menu open / close when rezise
-  let wasPhone = isPhone();
-  let reopen = false;
-
-  window.addEventListener("resize", () => {
-    const nowPhone = isPhone();
-
-    if (wasPhone && !nowPhone) {
-      reopen = menuOpen;
-      if (menuOpen) closeMenu();
-    }
-
-    if (!wasPhone && nowPhone) {
-      if (reopen) openMenu(false);
-    }
-
-    wasPhone = nowPhone;
-  });
-
-
-
-// Searchbar expand when focused
+// Searchbar expand when focused and retract when blurred
   const searchbar = document.getElementById("searchbar");
   const seachbarInput = document.getElementById("searchbar-input");
 
@@ -83,51 +67,3 @@
     container.classList.toggle("basket-open");
     container.classList.toggle("basket-closed");
   })
-
-
-
-
-
-
-
-
-// // Header shows and hides
-
-//     const mainHeader = document.getElementById("main-header");
-//     const gradient = document.getElementById("header-gradient");
-//     const mainHeaderHeight = mainHeader.offsetHeight;
-
-//     let lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;      
-//     let currentOffset = 0;
-//     let targetOffset = 0;
-//     let rafId = null;
-    
-//     function clamp(v, min, max)  {
-//         return Math.max(min, Math.min(max, v));
-//     }
-
-//     function animate() {
-//         currentOffset += (targetOffset - currentOffset) * 0.2;
-//         if (Math.abs(targetOffset - currentOffset) < 0.5) currentOffset = targetOffset;
-
-//         mainHeader.style.transform = `translateY(-${currentOffset}px)`;
-
-//         if (currentOffset !== targetOffset) {
-//             rafId = requestAnimationFrame(animate);
-//         } else {
-//             rafId = null;
-//         }
-//     }
-
-//     window.addEventListener("scroll", () => {
-//         const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-//         const delta = scrollTop - lastScrollTop;
-
-//         targetOffset = clamp(targetOffset + delta, 0, mainHeaderHeight);
-//         lastScrollTop = scrollTop;
-
-//         if (!rafId) rafId = this.requestAnimationFrame(animate);
-//     });
-
-
-
